cfc32fe1322aa58e9e5e21fcbd45653020939e15
Adrian Y <66759189+adryue@users.noreply.github.com>
2023-10-11 08:51:18 -0700

    edit db, link to new team page
    change some database entries, as well as added a link to the new team page

--- dbCreate.php
+++ dbCreate.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "123456";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -22,7 +22,7 @@ if ($conn->query($sql) === TRUE) {
 }*/
 $conn->query("USE myDB;");
 // Add people table
-$sql = "CREATE TABLE people (
+/*$sql = "CREATE TABLE people (
     ID int primary key auto_increment,
     fName varchar(20),
     lName varchar(20),
@@ -32,8 +32,8 @@ if ($conn->query($sql) === TRUE) {
 	echo "people table created successfully";
 } else {
 	echo "Error creating people table" . $conn->error;
-}
-
+}*/
+$conn->query("TRUNCATE TABLE people");
 // Add people
 $sql = "INSERT INTO people (fName, lName, bio)
 VALUES  ('Adrian', 'Yuen', 'I\'m Adrian. Some of my hobbies include playing the piano and coding. I learned programming from my dad in elementary school and middle school. My favorite language is C++.'),
@@ -41,7 +41,7 @@ VALUES  ('Adrian', 'Yuen', 'I\'m Adrian. Some of my hobbies include playing the 
         ('Daniel', 'Chen', 'Hi I\'m Daniel, and I\'m the sexiest Asian coder in the whole of my friend group (which only consists of me) my hobbies include procrastinating on APang homework and watching as my brain rots as I try to figure out how coding works.'),
         ('David', 'Zhi', 'I\'m David. I like watching sports and TV shows. Things I don\'t like doing are not watching sports and tv shows.'),
         ('Jeffrey', 'Wu', 'Hi I\'m Jeffrey. I like playing and watching watching basketball. My favorite team is the Warriors. In my offtime I enjoy crying over my homework or the state of my fantasy football team.'),
-        ('Pranav', 'Santhakumar', 'Hard-worker, adaptable & good at communicating. Coding skills: Learning python.');";
+        ('Pranav', 'Santhakumar', 'Santhakumar, Pranav: Hard-worker, adaptable & good at communicating. Coding skills: Learning python.');";
 if ($conn->query($sql) === TRUE) {
     echo "inserted values successfully";
 } else {
